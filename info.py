f735b1cbe9fae3ec56063c22ecbed42cfa655939
mikaelsahlin <mikael.hg.sahlin@gmail.com>
2017-03-28 14:50:25 +0200

    Update info.py

--- info.py
+++ info.py
@@ -1,2 +1,9 @@
 # DBN - Deep belief Network
 #
+import numpy
+import sys
+from HiddenLayer import HiddenLayer
+from LogisticRegression import LogisticRegression
+from RBM import RBM
+from utils import *
+
